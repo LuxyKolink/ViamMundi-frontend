065f982f91ee60df80e196cfb963360b830066f4
unknown <kevin.quintana.256bit@gmail.com>
2023-10-05 09:56:42 -0500

    test: Realizar prueba de conexión al servicio de autenticación

--- lib/src/services/auth.api.dart
+++ lib/src/services/auth.api.dart
@@ -1,14 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class authServices {
   Future<http.Response> test() async {
-    final response =
-        await http.delete(Uri.parse('http://192.168.56.1:4000/auth/'));
     if (kDebugMode) {
-      print(response);
       print("sad");
     }
-    return response;
+    try {
+      final response = await http.delete(
+          Uri.parse('http://192.168.56.1:4000/auth/delete'),
+          headers: {'Accept': 'application/json'});
+      return response;
+    } catch (e) {
+      print("pasó");
+      print(e.toString());
+      return http.Response('Error en la solicitud.', 500);
+    }
   }
 }

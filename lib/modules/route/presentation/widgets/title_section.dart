375348369837aad2093697914c901ad1474ddf5c
unknown <kevin.quintana.256bit@gmail.com>
2023-10-31 06:53:31 -0500

    feat: Complete route page

--- lib/modules/route/presentation/widgets/title_section.dart
+++ lib/modules/route/presentation/widgets/title_section.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:viammundi_frontend/shared/constants/constants.dart';
+import 'package:viammundi_frontend/shared/widgets/circle_avatar.dart';
 
-class TitleSection extends StatelessWidget {
+class CompleteRouteTitleSection extends StatelessWidget {
   final String profileImageUrl;
   final String userName;
   final String routeName;
 
-  const TitleSection({
+  const CompleteRouteTitleSection({
     super.key,
     required this.profileImageUrl,
     required this.userName,
@@ -35,47 +36,29 @@ class TitleSection extends StatelessWidget {
       padding: const EdgeInsets.all(AppSpacing.spacingMedium),
       child: Row(
         children: [
-          // Columna de la foto de perfil (Círculo)
-          Container(
-            width: 40.0,
-            height: 40.0,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: AssetImage(
-                    profileImageUrl), // Aquí puedes cargar la imagen de perfil
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
+          const CircleAvatarWidget(),
           const SizedBox(width: 16.0),
-
-          // Columna del nombre de la ruta (Ocupa más espacio)
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  userName,
+                  routeName,
                   style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 18.0,
                   ),
                 ),
-                Text(routeName),
+                Text(userName),
               ],
             ),
           ),
           const SizedBox(width: 16.0),
-
-          // Columna del botón de guardar
           IconButton(
-            icon: const Icon(Icons.save),
+            icon: const Icon(Icons.bookmark),
             onPressed: () => onSavePressed(context),
           ),
           const SizedBox(width: 16.0),
-
-          // Columna del botón de reportar
           IconButton(
             icon: const Icon(Icons.report),
             onPressed: () => onReportPressed(context),
--- lib/shared/widgets/circle_avatar.dart
+++ lib/shared/widgets/circle_avatar.dart
@@ -12,13 +12,13 @@ class CircleAvatarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: CircleAvatar(
-        //backgroundColor: Color.fromARGB(0, 28, 203, 142),
-        //radius: 55,
-        
-        backgroundImage: NetworkImage(profileImageUrl)
-            // ? NetworkImage(profileImageUrl!)
-            // : const AssetImage('assets/icons/profile_icon.png'),
-      ),
+          backgroundColor: AppColors.white,
+          //radius: 55,
+
+          backgroundImage: NetworkImage(profileImageUrl)
+          // ? NetworkImage(profileImageUrl!)
+          // : const AssetImage('assets/icons/profile_icon.png'),
+          ),
     );
   }
 }
